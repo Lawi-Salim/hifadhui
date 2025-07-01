9a8acc2a2774a71d6c50fcb9523d95c969ab0232
Lawi Salim <dahlawibrahim@gmail.com>
2025-07-01 10:36:09 +0000

    Ajout d'un log sur register.js

--- frontend/api/auth/register.js
+++ frontend/api/auth/register.js
@@ -1,5 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
+console.log("SUPABASE_URL", process.env.REACT_APP_SUPABASE_URL);
+console.log("SUPABASE_ANON_KEY", process.env.REACT_APP_SUPABASE_ANON_KEY);
+
 const supabase = createClient(
   process.env.REACT_APP_SUPABASE_URL,
   process.env.REACT_APP_SUPABASE_ANON_KEY
