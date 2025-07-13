ebb641e8c59c10c649343bdaa6598e16c62a237c
Lawi Salim <dahlawibrahim@gmail.com>
2025-07-13 18:27:39 +0000

    Fix singleton Supabase

--- frontend/src/supabaseClient.js
+++ frontend/src/supabaseClient.js
@@ -3,12 +3,11 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
 const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 
-// Fonction utilitaire pour obtenir un client contextuel
-export function getSupabaseClient(token) {
-  if (token) {
-    return createClient(supabaseUrl, supabaseAnonKey, {
-      global: { headers: { Authorization: `Bearer ${token}` } }
-    });
+let supabase;
+
+export function getSupabaseClient() {
+  if (!supabase) {
+    supabase = createClient(supabaseUrl, supabaseAnonKey);
   }
-  return createClient(supabaseUrl, supabaseAnonKey);
+  return supabase;
 } 
