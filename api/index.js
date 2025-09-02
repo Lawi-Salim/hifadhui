7d190b97fb283277a36675302f4690b68e0199a0
Lawi Salim <dahlawibrahim@gmail.com>
2025-09-02 22:32:11 +0000

    Toujours xécuter le script create-admin.js après le build

--- api/index.js
+++ api/index.js
@@ -1,11 +1,3 @@
 const app = require('../backend/server.js');
-const createAdmin = require('../backend/scripts/create-admin.js');
-
-// Exécuter le script create-admin au démarrage
-createAdmin().then(() => {
-  console.log('✅ Script create-admin exécuté');
-}).catch(error => {
-  console.error('❌ Erreur create-admin:', error);
-});
 
 module.exports = app;
